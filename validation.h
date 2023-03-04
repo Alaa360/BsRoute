9df82588292a689d1e75c6a5989ec1c7597f3019
MinaNarouz <36604295+MinaNarouz@users.noreply.github.com>
2023-03-04 18:27:06 +0200

    Update validation.h

--- validation.h
+++ validation.h
@@ -1,14 +1,85 @@
-#include "validation.h"
+#include <iostream>
+#include <ctype.h>
+#include <stdlib.h>
+#include<string>
+#include <limits>
 
-class Validation
-{
-    static bool checkName(string name) {}
-    static bool checkPassword(string password) {}
+//#include "validation.h"
 
+using namespace std;
+
+class Validation {
 public:
-    static string insertName() {}
-    static string insertPassword() {}
+    static bool checkName(string name) {
+        if (name.length() < 5 || name.length() > 20) {
+            return false;
+        }
+        for (char c : name) {
+            if (!isalpha(c) && !isspace(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    static bool checkPassword(string password) {
+        if (password.length() < 8 || password.length() > 20) {
+            return false;
+        }
+        return true;
+    }
 
-    static double insertBalance() {}
-    static double insertSalary() {}
+    static string insertName() {
+        string name;
+        cout << "Insert your First and Last Name: ";
+        getline(cin, name);
+        while (!checkName(name)) {
+            system("clear");
+            cout << "Error, Insert name not less than 5 and not greater than 20 characters \n";
+            cout << "Insert your First and Last Name: ";
+            getline(cin, name);
+        }
+        return name;
+    }
+    static string insertPassword() {
+        string password;
+        cout << "Insert your password: ";
+        cin >> password;
+        while (!checkPassword(password)) {
+            system("clear");
+            cout << "Error, Password length not less than 8 and not greater than 20 \n";
+            cout << "Insert your password: ";
+            cin >> password;
+        }
+        return password;
+    }
+
+    static double insertBalance() {
+        double balance;
+        cout << "Insert Balance: ";
+        cin >> balance;
+        while (cin.fail() || balance < 1500) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            system("clear");
+            cout << "Error, Balance not less than 1500 \n";
+            cout << "Insert Balance: ";
+            cin >> balance;
+        }
+        return balance;
+    }
+
+    static double insertSalary() {
+        double salary;
+        cout << "Insert Salary: ";
+        cin >> salary;
+        while (cin.fail() || salary < 5000) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            system("clear");
+            cout << "Error, Salary not less than 5000 \n";
+            cout << "Insert Salary: ";
+            cin >> salary;
+        }
+        return salary;
+    }
 };
