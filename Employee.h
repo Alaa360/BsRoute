06fbee6cc610c4d2707ce9376fe1c4e07e0dd7d7
MinaNarouz <36604295+MinaNarouz@users.noreply.github.com>
2023-03-06 22:48:58 +0200

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,8 +1,10 @@
 #pragma once
 #include"Person.h"
-#include "Validation.h"
 #include"IDGenerator.h"
-#include<fstream>
+
+
+//Employee Class inherited everything from CLass Person
+// Employee Unique Variable is Salary
 
 class Employee : public Person{
 private:
@@ -17,35 +19,26 @@ public:
 	Employee(string name, string password, double salary) : Person(name, password) {
 		this->salary = salary;
 	}
+	
 	// Setters
-	void setName() {
-		this->name = Validation::insertName();
+
+	void setSalary(double salary) {
+		this->salary = salary;
 	}
-	void setPassword() {
-		this->password = Validation::insertPassword();
-	}
-	int getID() {
-		return this->id;
-	}
-	string getName() {
-		return this->name;
-	}
-	string getPassword() {
-		return this->password;
-	}
+
 	double getSalary() {
 		return this->salary;
 	}
+
+	int getID() {
+		return this->id;
+	}
+
 	void display() {
 		cout << "Employee ID: " << getID() << endl;
 		cout << "Employee Name: " << getName() << endl;
 		cout << "Employee Salary: " << getSalary() << endl;
-
 	}
-
-
 };
 
 IDGenerator Employee::idGenerator(1000);
-
-
